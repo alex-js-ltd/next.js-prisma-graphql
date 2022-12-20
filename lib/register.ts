a2044c133883058117650dd315a9d053e9353847
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-20 21:02:20 +0100

    update

--- lib/register.ts
+++ lib/register.ts
@@ -2,6 +2,7 @@ import request from 'graphql-request'
 import { useMutation } from '@tanstack/react-query'
 import { graphql } from 'graphql/client/gql'
 import { MutationRegisterArgs } from 'graphql/client/graphql'
+import { useRouter } from 'next/router'
 
 const registerMutationDocument = graphql(/* GraphQL */ `
   mutation register($email: String!, $password: String!) {
@@ -16,6 +17,8 @@ const registerMutationDocument = graphql(/* GraphQL */ `
 `)
 
 function useRegister() {
+  const router = useRouter()
+
   return useMutation({
     mutationFn: ({ email, password }: MutationRegisterArgs) =>
       request('http://localhost:3000/api/graphql', registerMutationDocument, {
@@ -25,6 +28,8 @@ function useRegister() {
 
     onSuccess(data) {
       console.log(data)
+
+      router.push('/books')
     },
   })
 }
