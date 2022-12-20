b8aea7f30b5b0b91fe894bb176f683f64a352023
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-20 20:52:57 +0100

    update

--- lib/register.ts
+++ lib/register.ts
@@ -1,6 +1,9 @@
-import { gql } from '@apollo/client'
+import request from 'graphql-request'
+import { useMutation } from '@tanstack/react-query'
+import { graphql } from 'graphql/client/gql'
+import { MutationRegisterArgs } from 'graphql/client/graphql'
 
-export const REGISTER = gql`
+const registerMutationDocument = graphql(/* GraphQL */ `
   mutation register($email: String!, $password: String!) {
     register(email: $email, password: $password) {
       createdAt
@@ -10,4 +13,15 @@ export const REGISTER = gql`
       updatedAt
     }
   }
-`
+`)
+
+function useRegister() {
+  return useMutation({
+    mutationFn: ({ email, password }: MutationRegisterArgs) =>
+      request('http://localhost:3000/api/graphql', registerMutationDocument, {
+        email,
+        password,
+      }),
+  })
+}
+export { useRegister }
