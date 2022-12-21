b6218b101020912afcea267871b77f7fabd482dc
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-21 20:32:14 +0100

    update

---
+++ comps/layout.tsx
@@ -0,0 +1,83 @@
+/** @jsxImportSource @emotion/react */
+import { Fragment, ReactElement } from 'react'
+
+import { Button, StyledLink } from 'comps/lib'
+import * as mq from 'styles/media-queries'
+import * as colors from 'styles/colors'
+
+const Layout = ({ children }: { children: ReactElement }) => {
+  return (
+    <Fragment>
+      <div
+        css={{
+          display: 'flex',
+          alignItems: 'center',
+          position: 'absolute',
+          top: '10px',
+          right: '10px',
+        }}
+      >
+        user
+        <Button variant="secondary" css={{ marginLeft: '10px' }}>
+          Logout
+        </Button>
+      </div>
+      <div
+        css={{
+          margin: '0 auto',
+          padding: '4em 2em',
+          maxWidth: '840px',
+          width: '100%',
+          display: 'grid',
+          gridGap: '1em',
+          gridTemplateColumns: '1fr 3fr',
+          [mq.small]: {
+            gridTemplateColumns: '1fr',
+            gridTemplateRows: 'auto',
+            width: '100%',
+          },
+        }}
+      >
+        <div css={{ position: 'relative' }}>
+          <Nav />
+        </div>
+        <main css={{ width: '100%' }}>{children}</main>
+      </div>
+    </Fragment>
+  )
+}
+
+export default Layout
+
+const Nav = () => (
+  <nav
+    css={{
+      position: 'sticky',
+      top: '4px',
+      padding: '1em 1.5em',
+      border: `1px solid ${colors.gray10}`,
+      borderRadius: '3px',
+      [mq.small]: {
+        position: 'static',
+        top: 'auto',
+      },
+    }}
+  >
+    <ul
+      css={{
+        listStyle: 'none',
+        padding: '0',
+      }}
+    >
+      <li>
+        <StyledLink href="/reading-list">Reading List</StyledLink>
+      </li>
+      <li>
+        <StyledLink href="/finished">Finished Books</StyledLink>
+      </li>
+      <li>
+        <StyledLink href="/books">Discover</StyledLink>
+      </li>
+    </ul>
+  </nav>
+)
--- pages/books.tsx
+++ pages/books.tsx
@@ -1,11 +1,16 @@
-import type { NextPage } from 'next'
+import type { ReactElement } from 'react'
+import Layout from 'comps/layout'
+import type { NextPageWithLayout } from './_app'
 import { useBooks } from 'lib/get-books'
 
-const Books: NextPage = () => {
+const Page: NextPageWithLayout = () => {
   const { data } = useBooks()
-
   console.log(data)
-  return <div>Books</div>
+  return <p></p>
 }
 
-export default Books
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>
+}
+
+export default Page
