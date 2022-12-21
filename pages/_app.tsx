8b6a3d2ecac12264b26b9b141a293000a8dd4f69
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-21 02:30:04 +0100

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap-reboot.css'
 import 'styles/global.css'
 import type { AppProps } from 'next/app'
-import { UserProvider } from '@auth0/nextjs-auth0/client'
 import { useState } from 'react'
 import {
   Hydrate,
@@ -14,13 +13,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <UserProvider>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <Component {...pageProps} />
-        </Hydrate>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </UserProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
