e09c3e41ec69e6e43a116acf9404fc44e8d8e578
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-21 20:24:17 +0100

    update

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
+export function middleware(request: NextRequest) {
   const token = request.cookies.get('ACCESS_TOKEN')?.value
 
   //redirect to login page
