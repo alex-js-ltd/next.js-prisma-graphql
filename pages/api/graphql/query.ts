45c0ba538ef5234235e49b8b2a6496cd37ef58cd
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-22 03:03:20 +0100

    updaTE

--- pages/api/graphql/query.ts
+++ pages/api/graphql/query.ts
@@ -4,6 +4,8 @@ const Query: QueryResolvers = {
   async books(_parent, args, ctx) {
     const { query } = args
 
+    if (!ctx.user) return null
+
     const books = await ctx.prisma.book.findMany({
       where: {
         title: {
