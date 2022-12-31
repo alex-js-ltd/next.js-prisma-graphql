c0af0447fc0436532bfb6c42926ee5b759bb0fe9
Alex Gilbert <alex.js.ltd@gmail.com>
2022-12-31 13:50:04 +0100

    update

--- pages/finished.tsx
+++ pages/finished.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import type { NextPageWithLayout } from './_app'
+import type { ReactElement } from 'react'
+import type { ListItem } from 'generated/graphql'
+import Layout from 'comps/layout'
+import { ListItemList } from 'comps/list-item-list'
+import { useListItems } from 'utils/list-items.client'
 
-const Finished = () => {
-  return <div>Finished</div>
+const Finished: NextPageWithLayout = () => {
+  const list = useListItems()
+
+  const filter = list.filter((li: ListItem) => Boolean(li.finishDate))
+
+  return <ListItemList list={filter} />
 }
 
+Finished.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>
+}
 export default Finished
--- pages/reading-list.tsx
+++ pages/reading-list.tsx
@@ -1,14 +1,14 @@
+import type { NextPageWithLayout } from './_app'
 import type { ReactElement } from 'react'
 import type { ListItem } from 'generated/graphql'
-import { StyledLink } from 'comps/lib'
 import Layout from 'comps/layout'
 import { ListItemList } from 'comps/list-item-list'
 import { useListItems } from 'utils/list-items.client'
 
-const ReadingList = () => {
+const ReadingList: NextPageWithLayout = () => {
   const list = useListItems()
 
-  const filter = list.filter((li: ListItem) => li.finishDate === null)
+  const filter = list.filter((li: ListItem) => !li.finishDate)
 
   return <ListItemList list={filter} />
 }
