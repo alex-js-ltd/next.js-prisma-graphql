bb3041fc771e060b7c7afb187f82bb555869c74a
Alex Gilbert <alex.js.ltd@gmail.com>
2023-01-02 11:07:15 +0100

    update

--- pages/book/[id].tsx
+++ pages/book/[id].tsx
@@ -1,9 +1,81 @@
+import type { ReactElement } from 'react'
+import type { NextPageWithLayout } from 'pages/_app'
+import Layout from 'comps/layout'
 import { useRouter } from 'next/router'
+import * as mq from 'styles/media-queries'
+import * as colors from 'styles/colors'
+
+import { useBook } from 'utils/books.client'
 
 const Book = () => {
   const router = useRouter()
   const { id } = router.query
-  return <div>Book {id}</div>
+
+  const book = useBook(id)
+
+  const { coverImageUrl, title, author, publisher, synopsis } = book
+
+  return (
+    <div>
+      <div
+        css={{
+          display: 'grid',
+          gridTemplateColumns: '1fr 2fr',
+          gridGap: '2em',
+          marginBottom: '1em',
+          [mq.small]: {
+            display: 'flex',
+            flexDirection: 'column',
+          },
+        }}
+      >
+        <img
+          src={coverImageUrl}
+          alt={`${title} book cover`}
+          css={{ width: '100%', maxWidth: '14rem' }}
+        />
+        <div>
+          <div css={{ display: 'flex', position: 'relative' }}>
+            <div css={{ flex: 1, justifyContent: 'space-between' }}>
+              <h1>{title}</h1>
+              <div>
+                <i>{author}</i>
+                <span css={{ marginRight: 6, marginLeft: 6 }}>|</span>
+                <i>{publisher}</i>
+              </div>
+            </div>
+            <div
+              css={{
+                right: 0,
+                color: colors.gray80,
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-around',
+                minHeight: 100,
+              }}
+            >
+              {/* {isLoading(book) ? null : <StatusButtons book={book} />} */}
+            </div>
+          </div>
+          <div css={{ marginTop: 10, height: 46 }}>
+            {/* {listItem && isFinished(listItem) ? (
+          <Rating listItem={listItem} />
+        ) : null}
+        {listItem ? <ListItemTimeframe listItem={listItem} /> : null} */}
+          </div>
+          <br />
+          <p>{synopsis}</p>
+        </div>
+      </div>
+      {/* {!isLoading(book) && listItem ? (
+    <NotesTextarea listItem={listItem} />
+  ) : null} */}
+    </div>
+  )
+}
+
+Book.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>
 }
 
 export default Book
--- utils/books.client.ts
+++ utils/books.client.ts
@@ -2,6 +2,7 @@ import { req } from './request.client'
 import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { graphql } from 'generated/gql'
 import type { Book } from 'generated/graphql'
+import bookPlaceholderSvg from 'assets/book-placeholder.svg'
 
 function useBooks(query: string) {
   const queryClient = useQueryClient()
@@ -22,13 +23,13 @@ function useBooks(query: string) {
   return { ...result, books: result?.data?.books ?? [] }
 }
 
-function useBook(id: string | undefined) {
-  const result = useQuery<Book | null, Error>({
+function useBook(id: string | string[] | undefined) {
+  const result = useQuery<Book, Error>({
     queryKey: ['book', id],
     queryFn: () => req(bookQueryDocument, { id }),
   })
 
-  return result?.data ?? null
+  return result?.data ?? loadingBook
 }
 
 export { useBooks, useBook }
@@ -60,3 +61,14 @@ const bookQueryDocument = graphql(/* GraphQL */ `
     }
   }
 `)
+
+const loadingBook = {
+  id: 0,
+  title: 'Loading...',
+  author: 'loading...',
+  coverImageUrl: bookPlaceholderSvg,
+  publisher: 'Loading Publishing',
+  synopsis: 'Loading...',
+  pageCount: 0,
+  loadingBook: true,
+}
